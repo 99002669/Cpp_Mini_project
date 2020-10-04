84c9a7d8f5d04600564b4edd13c192bec09c0cf9
99002669 <71541637+99002669@users.noreply.github.com>
2020-10-04 22:02:39 +0530

    CMake Files

---
+++ SongStore/CMakeFiles/SongMain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SongMain.dir/song.cpp.o"
+  "SongMain.pdb"
+  "SongMain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SongMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
