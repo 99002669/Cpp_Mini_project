95dd01221030d388f710e7133803b98e4d0114d3
99002669 <71541637+99002669@users.noreply.github.com>
2020-10-17 14:10:45 +0530

    Adding comments as per #3

--- SongStore/song-test.cpp
+++ SongStore/song-test.cpp
@@ -17,21 +17,21 @@ class SongTest : public ::testing::Test {
     Song *ptr;
 };
 
-TEST_F(SongTest, DefaultConstructor) {
+TEST_F(SongTest, DefaultConstructor) {   //default constructor 
     SongGenre ft;
     EXPECT_EQ(0, ft.getID());
     EXPECT_EQ(0, ft.getGenre().length());
     EXPECT_EQ(0, ft.getName().length());
     EXPECT_EQ(0, ft.getPrice());
 }
-TEST_F(SongTest, ParameterizedConstructor) {
+TEST_F(SongTest, ParameterizedConstructor) { //parameterized constructor
     SongGenre *fptr = dynamic_cast<SongGenre*>(ptr);
     EXPECT_EQ( 1, fptr->getID());
     EXPECT_STREQ("Pop Rock", fptr->getGenre().c_str());
     EXPECT_STREQ("Believer", fptr->getName().c_str());
     EXPECT_EQ(23.00, fptr->getPrice());
 }
-TEST_F(SongTest, AnotherTest) {
+TEST_F(SongTest, AnotherTest) { 
     SongGenre ft(2,"Despacito", 30.00, "Luis Fonsi", "Latin Pop","2017");
     Song *pt = &ft;
     EXPECT_EQ( 2, ft.getID());
