a388397ed7b62084ea105687e88e24d70cb27797
99002669 <71541637+99002669@users.noreply.github.com>
2020-10-17 14:17:47 +0530

    This fixes #3

--- SongStore/song.cpp
+++ SongStore/song.cpp
@@ -1,5 +1,5 @@
 #include"song.h"
 #include <string>
 
-Song::Song():ID(0),name(""),price(0.0), artist("") {}
-Song::Song(int ID, std::string name, double price, std::string artist) : ID(ID),name(name),price(price), artist(artist) {}
+Song::Song():ID(0),name(""),price(0.0), artist("") {} //default constructor
+Song::Song(int ID, std::string name, double price, std::string artist) : ID(ID),name(name),price(price), artist(artist) {} //parameterized constructor
