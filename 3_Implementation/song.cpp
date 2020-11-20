560d25680dd13a0f194914a84aa315e95aa30d5b
99002669 <71541637+99002669@users.noreply.github.com>
2020-11-20 12:02:36 +0530

    Update song.cpp

--- 3_Implementation/song.cpp
+++ 3_Implementation/song.cpp
@@ -1,5 +1,6 @@
 #include"song.h"
 #include <string>
+using namespace std;
 
 Song::Song():ID(0),name(""),price(0.0), artist("") {} //default constructor
-Song::Song(int ID, std::string name, double price, std::string artist) : ID(ID),name(name),price(price), artist(artist) {} //parameterized constructor
+Song::Song(int ID, const name, double price, std::string artist) : ID(ID),name(name),price(price), artist(artist) {} //parameterized constructor
